efbf5e9af430906a2d09def5cd43e40ea769140f
evfristo <evfristo@ksu.edu>
2021-10-19 14:28:59 -0900

    Fixed

--- pwm-motor/pwm-motor-rpi/motor-example.py
+++ pwm-motor/pwm-motor-rpi/motor-example.py
@@ -1,7 +1,10 @@
-from RPI import GPIO
+from RPi import GPIO
 from time import sleep
 PWM_PIN = 19
 DIR_PIN = 16
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(PWM_PIN, GPIO.OUT)
 GPIO.setup(DIR_PIN, GPIO.OUT)
@@ -9,16 +12,17 @@ pwmPin = GPIO.PWM(PWM_PIN, 1000)
 pwmPin.ChangeDutyCycle(100)
 pwmPin.start(0)
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
 
 direction = GPIO.LOW
 
 while True:
     GPIO.output(DIR_PIN, direction)
+    print("Running at speed ", 100)
     pwmPin.ChangeDutyCycle(100)
     sleep(5)
+    print("Running at speed ", 50)
     pwmPin.ChangeDutyCycle(50)
     sleep(5)
+    print("Running at speed ", 0)
     pwmPin.ChangeDutyCycle(0)
     sleep(2)
